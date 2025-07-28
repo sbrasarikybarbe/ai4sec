41bac8c7fe87ebd2da6775904e686d6f74db6eae
sbrasarikybarbe <threeofuschannel@gmail.com>
2025-07-28 21:56:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import os
 from modules import parser, gemini_api, risk_classifier, mitigations, dashboard
 
 st.set_page_config(page_title="AI4Sec", layout="wide")
@@ -8,11 +7,9 @@ st.title("🔐 AI4Sec – AI-powered Security Report Analyzer")
 uploaded_file = st.file_uploader("Carica un report di sicurezza (JSON, XML, TXT)", type=["json", "xml", "txt"])
 if uploaded_file:
     report_data = parser.parse_report(uploaded_file)
-    st.success("API KEY:")
-    st.success("Report caricato e analizzato.")
-    st.success(os.getenv("GEMINI_API_KEY"))
+    st.success("Report succesfully loaded.")
 
-    with st.spinner("Analisi AI in corso..."):
+    with st.spinner("AI analysis running..."):
         ai_analysis = gemini_api.analyze_report(report_data)
 
     risks = risk_classifier.classify_risks(ai_analysis)

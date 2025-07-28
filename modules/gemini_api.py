d67d52e7b2cec65203f88ccb72bd97ecc1ada0e4
sbrasarikybarbe <threeofuschannel@gmail.com>
2025-07-28 21:46:03 +0200

    Update gemini_api.py

--- modules/gemini_api.py
+++ modules/gemini_api.py
@@ -2,10 +2,8 @@
 import os
 import google.generativeai as genai
 
-print("API KEY:", os.getenv("GEMINI_API_KEY"))
 api_key = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=api_key)
-
 
 def analyze_report(report_data):
     prompt = f"Analizza questo report di sicurezza e identifica i rischi: {report_data}"

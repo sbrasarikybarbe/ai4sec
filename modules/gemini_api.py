119220437a403496a5e032ef1d65c6c02b352c35
sbrasarikybarbe <threeofuschannel@gmail.com>
2025-07-28 20:55:22 +0200

    Update gemini_api.py

--- modules/gemini_api.py
+++ modules/gemini_api.py
@@ -8,6 +8,6 @@ genai.configure(api_key=api_key)
 
 def analyze_report(report_data):
     prompt = f"Analizza questo report di sicurezza e identifica i rischi: {report_data}"
-    model = genai.GenerativeModel("gemini-pro")
+    model = genai.GenerativeModel("gemini-2.0-flash")
     response = model.generate_content(prompt)
     return response.text

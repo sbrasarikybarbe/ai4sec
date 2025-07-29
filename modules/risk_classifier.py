61048e4ed8071382bb35d896cbea553aca87dd2c
sbrasarikybarbe <threeofuschannel@gmail.com>
2025-07-29 10:19:13 +0200

    Delete modules directory

--- modules/risk_classifier.py
+++
@@ -1,8 +0,0 @@
-def classify_risks(ai_analysis):
-    risks = []
-    for line in ai_analysis.split("\n"):
-        if any(keyword in line.lower() for keyword in ["spoofing", "tampering", "repudiation", "information disclosure", "denial of service", "elevation of privilege"]):
-            risks.append({"category": "STRIDE", "description": line})
-        elif any(keyword in line.lower() for keyword in ["injection", "broken authentication", "sensitive data exposure", "security misconfiguration"]):
-            risks.append({"category": "OWASP", "description": line})
-    return risks
